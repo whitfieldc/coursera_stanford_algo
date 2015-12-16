3a5b1056df0c604d5428484fe04215aa9be06bd3
Caitlyn Yu <caitlynmyu@gmail.com>
2015-12-15 22:13:17 -0800

    working javascript solution

--- week_1/merge_sort_cait.js
+++ week_1/merge_sort_cait.js
@@ -4,45 +4,52 @@ input_array3 = []
 input_array4 = [1]
 
 
-
-
 function splitSort(array) {
-  var total_length = array.length
-  console.log(total_length)
-  if (total_length < 2) {
+  var length = array.length
+  var midpoint = Math.floor(length / 2)
+  if (length < 2) {
     return array
   } else {
     // split and recurse, then merge
-    var midpoint = total_length / 2
-    var left = splitSort(0, midpoint)
-    var right = splitSort(midpoint, total_length)
-    return merge(array, left, right)
+    var left = splitSort(array.slice(0, midpoint))
+    var right = splitSort(array.slice(midpoint, length))
+    return merge(left, right)
   }
 }
 
-function merge(array, left, right) {
+function merge(left, right) {
+  left = left || []
+  right = right || []
+  var total_length = left.length + right.length
   var merged = []
-  var i = 0
-  var j = 0
-  while (merged.length < array.length) {
-    switch (left[i] < right[j]) {
-      case true:
-        merged.push(left[i])
-        i++
-        break;
-      case false:
-        merged.push(right[j])
-        j++
-        break;
+  var i = 0, j = 0
+  while (merged.length < total_length) {
+    if (left[i] && right[j]) {
+      switch(left[i] < right[j]) {
+        case true:
+          merged.push(left[i])
+          i++
+          break;
+        case false:
+          merged.push(right[j])
+          j++
+          break;
+      }
+    } else if (left[i]) {
+      merged.push(left[i])
+      i++
+    } else if (right[j]) {
+      merged.push(right[j])
+      j++
     }
   }
   return merged
 }
 
-splitSort(input_array)
-// splitSort(input_array2)
-// splitSort(input_array3)
-// splitSort(input_array4)
+console.log(splitSort(input_array))
+console.log(splitSort(input_array2))
+console.log(splitSort(input_array3))
+console.log(splitSort(input_array4))
 
 
 
